6a1dcecab2ec87b8991b1fbbb2b3ad360b671ba8
Rod Hentringer <67014030+rodhent@users.noreply.github.com>
2020-10-13 21:47:51 -0400

    package updates

---
+++ src/components/LoginBtn.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useAuth0 } from '@auth0/auth0-react'
+
+const LoginBtn = () => {
+    const { loginWithRedirect } = useAuth0()
+    return (
+        <button onClick={() => loginWithRedirect()}>login</button>
+     
+    )
+}
+
+export default LoginBtn
