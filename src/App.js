33b6acade0fa402b106f063dd0dc13cf5e5c1388
Rod Hentringer <67014030+rodhent@users.noreply.github.com>
2020-10-13 21:48:26 -0400

    app.js updated

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import React from 'react';
 
 import './App.css';
 import LoginBtn from './components/LoginBtn.jsx'
+import LogoutBtn from './components/LogoutBtn';
+import Profile from './components/Profile.jsx'
+import {useAuth0} from '@auth0/auth0-react'
 
 function App() {
+  const {isLoading} = useAuth0()
+
+  if (isLoading) return <div>Loading...</div>
+
   return (
-    <>
+    <div>
       <LoginBtn />
-    </>
+      <LogoutBtn />
+      <Profile />
+    </div>
   );
 }
 
